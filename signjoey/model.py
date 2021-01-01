1b9ceb706d7df3b35d3b6e7c817df289d5363165
SamuelDiai <diai.samuel@gmail.com>
2021-01-02 00:34:02 +0100

    .

--- signjoey/model.py
+++ signjoey/model.py
@@ -119,9 +119,12 @@ class SignModel(nn.Module):
             # Gloss Recognition Part
             # N x T x C
             if self.fusion_type == 'late_fusion':
+                print("ENCODER MERGE : ", torch.cat([encoder_output, encoder_output_pose], dim=2))
                 gloss_scores = self.gloss_output_layer(torch.cat([encoder_output, encoder_output_pose], dim = 2))
             else :
                 gloss_scores = self.gloss_output_layer(encoder_output)
+
+            print(gloss_scores)
             # N x T x C
             gloss_probabilities = gloss_scores.log_softmax(2)
             # Turn it into T x N x C
